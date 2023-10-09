9a139bc8c880c7fe2096d40ca9d9e9824872a0fc
Danila Kharchenko <spook9400@mail.ru>
2023-10-09 18:38:41 +0300

    Здравствуйте, выполнил ДЗ Add commit

---
+++ catalog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from catalog import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:pk>', views.product, name='product')
+]
+
